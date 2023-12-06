18da5dd645e16cd2e0247d7f9c670a7876f60331
FilippoBersan <filippobersan99@gmail.com>
2023-12-06 15:28:57 +0100

    feat: esercizio 1 + bonus jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'Jumbotron',
+  data() {
+    return {
+      text: 'Jumbotron x bonus',
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="container-jumbo">
+      <h2>{{ text }}</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: rgb(187, 121, 34);
+}
+.container {
+  display: flex;
+  justify-content: center;
+}
+.container-jumbo {
+  border-radius: 1%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(240, 248, 255, 0.612);
+  width: 50%;
+  height: 18.75rem;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'App Component',
+  data() {
+    return {
+      text: 'La mia prima app con Vite!',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  padding: 1.875rem;
+}
+</style>
